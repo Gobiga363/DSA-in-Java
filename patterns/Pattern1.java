09996c4f3157a69cb18202f75c639082896316f5
Gobiga <110178279+Gobiga363@users.noreply.github.com>
2024-09-09 23:45:28 +0530

    Update Pattern1.java

--- patterns/Pattern1.java
+++ patterns/Pattern1.java
@@ -1,19 +1,15 @@
 package patterns;
-
 public class Pattern1 {
 
 	public static void main(String[] args) {
 		int n=4;
 		
 		for(int i = 0; i< n*n; i++){
-            System.out.print("*");
+            		System.out.print("*");
 
-            //If n, prints new line
-            if((i+1) % n ==0){
-                System.out.println();
-            }
-        }
-
+           	//If n, prints new line
+            	if((i+1) % n ==0){
+                	System.out.println();}
+        	}
 	}
-
 }
